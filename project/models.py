3e732a4bc9bfacc48815b276366523163b92704f
Andrey Popov <popov.a39rus@gmail.com>
2022-03-09 11:03:49 +0200

    first

--- project/models.py
+++ project/models.py
@@ -5,9 +5,10 @@ import uuid
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    id = models.UUIDField(uuid=uuid.uuid4(), unique=True, primary_key=True,
-                          editable=False, null=False)
+    id = models.UUIDField(default=uuid.uuid4(), unique=True,
+                          primary_key=True, editable=False)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+
 
